9e77f69ecaddca27b198a621996913d68e04fa90
dlindenbaum <dlindenbaum@iqt.org>
2017-03-25 01:56:13 -0400

    added python/test.py to do import test

---
+++ python/test.py
@@ -0,0 +1,4 @@
+from spaceNetUtilities import geoTools as gT
+from spaceNetUtilities import labelTools as lT
+from spaceNetUtilities import dataTools as dT
+from spaceNetUtilities import evalTools as lT
